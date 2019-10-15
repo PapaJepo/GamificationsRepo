4939417ba7c1b1bfbf3a64d138f146807091a682
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2019-10-15 18:42:10 +0100

    Pet Name Input saves

---
+++ Medical Compliance/Assets/Scripts/NameInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameInput : MonoBehaviour
+{
+    public InputField Name_Input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Name_Input.text = PlayerPrefs.GetString("NameKey");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void UserInput(string Name)
+    {
+        Debug.Log(Name);
+        PlayerPrefs.SetString("NameKey", Name);
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetString("NameKey", Name_Input.text);
+    }
+}
