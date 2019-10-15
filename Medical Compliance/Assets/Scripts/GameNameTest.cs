559c76c882b11e1ac4653959424f36248a18c4cb
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2019-10-15 19:08:13 +0100

    Test name saving across scenes

---
+++ Medical Compliance/Assets/Scripts/GameNameTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameNameTest : MonoBehaviour
+{
+    public Text NameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NameText.text = PlayerPrefs.GetString("NameKey");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
