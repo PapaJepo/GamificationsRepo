4560554b5aa966071e969df85868cfeb373da2fc
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2019-11-02 22:32:12 +0000

    Testing Screen Orientation

---
+++ Medical Compliance/Assets/Scripts/ScreenOrientation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenOrientation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+      //  Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
