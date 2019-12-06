3644a1ad1273ad5a5be4469a651fb4e8b0b1aaa4
SpookyBoi <aaronnk1999@gmail.com>
2019-12-06 13:40:24 +0000

    Minor edits and added script to meditation anim

---
+++ Medical Compliance/Assets/Scripts/MeditationAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeditationAnim : MonoBehaviour
+{
+    public GameObject Pet;
+    public Animator PetAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PetAnim.SetBool("Meditate", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
